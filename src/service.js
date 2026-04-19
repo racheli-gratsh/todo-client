54062c8941cc1284c1aa0d8ba890d784f623ee68
Rachel gratsh <r27175847@gmail.com>
2026-04-19 15:19:23 +0300

    fix servise

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://todo-api-oyhv.onrender.com' });
+const api = axios.create({ baseURL:'https://todo-api-oyhv.onrender.com' });
 
 api.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
